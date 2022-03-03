7dba758943e852e0e5e4e58f4fc2c0d8daaab14e
unknown <yumei4466@163.com>
2022-03-03 19:50:57 +0800

    登录页面

---
+++ react-travel/src/pages/signIn/SignInForm.tsx
@@ -0,0 +1,75 @@
+import { Form, Input, Button, Checkbox } from 'antd';
+import styles from './SignInForm.module.css';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+export const SignInForm = () => {
+  const history = useHistory();
+  const onFinish = async (values: any) => {
+    console.log('Success:', values);
+    try {
+      await axios.post('http://123.56.149.216:8080/auth/register', {
+        email: values.username,
+        password: values.password,
+        confirmPassword: values.confirm,
+      });
+      history.push('/signIn/');
+    } catch (error) {
+      alert('注册失败！');
+    }
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      className={styles['register-form']}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}
+        name="remember"
+        valuePropName="checked"
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
--- react-travel/src/pages/signIn/SignInPage.tsx
+++ react-travel/src/pages/signIn/SignInPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './SignInPage.module.css';
 import { UserLayout } from '../../layouts/userLayout';
+import { SignInForm } from './SignInForm';
 
 export const SignInPage: React.FC = (props) => {
-  console.log(props)
   return (   
     <UserLayout>
-      <h1>登录页面</h1>
+      <SignInForm />
     </UserLayout> 
   );
 }

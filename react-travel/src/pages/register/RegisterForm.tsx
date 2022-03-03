347729be3ad9ac7433ee24ee90a476ca498d5190
unknown <yumei4466@163.com>
2022-03-03 14:57:20 +0800

    注册业务逻辑处理

--- react-travel/src/pages/register/RegisterForm.tsx
+++ react-travel/src/pages/register/RegisterForm.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import styles from './RegisterForm.module.css';
 import { Form, Input, Button, Checkbox } from 'antd';
+import styles from './RegisterForm.module.css';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 export const RegisterForm = () => {
-  const onFinish = (values: any) => {
+  const history = useHistory();
+
+  const onFinish = async (values: any) => {
     console.log('Success:', values);
+    try {
+      await axios.post('http://123.56.149.216:8080/auth/register', {
+        email: values.username,
+        password: values.password,
+        confirmPassword: values.confirm,
+      });
+      history.push('/signIn/');
+    } catch (error) {
+      alert('注册失败！');
+    }
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -13,13 +34,11 @@ export const RegisterForm = () => {
 
   return (
     <Form
+      {...layout}
       name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
       initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
-      autoComplete="off"
       className={styles['register-form']}
     >
       <Form.Item
@@ -40,28 +59,28 @@ export const RegisterForm = () => {
 
       <Form.Item
         label="Confirm Password"
-        name="Confirm"
+        name="confirm"
         hasFeedback
         rules={[
-          { required: true, message: 'Please input your Confirm password!' },
-          (({getFieldValue}) => ({
+          { required: true, message: 'Please input your confirm password!' },
+          ({ getFieldValue }) => ({
             validator(_, value) {
-              if(!value || getFieldValue('password') === value) {
-                return Promise.resolve()
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
               }
-              return Promise.reject('密码确认不一致')
-            }
-          }))
+              return Promise.reject('密码确认不一致！');
+            },
+          }),
         ]}
       >
         <Input.Password />
       </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
         <Checkbox>Remember me</Checkbox>
       </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Form.Item {...tailLayout}>
         <Button type="primary" htmlType="submit">
           Submit
         </Button>
@@ -69,10 +88,3 @@ export const RegisterForm = () => {
     </Form>
   );
 };
-
-// export const RegisterForm: React.FC = (props) => {
-//   console.log(props)
-//   return (
-//     <h1>RegisterForm</h1> 
-//   );
-// }

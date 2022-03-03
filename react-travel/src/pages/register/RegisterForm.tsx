1b1d778a598487ec57eb4c26fe52f740ab9420f9
unknown <yumei4466@163.com>
2022-03-03 14:08:52 +0800

    feat: 注册页面 Antd+ts表单处理

--- react-travel/src/pages/register/RegisterForm.tsx
+++ react-travel/src/pages/register/RegisterForm.tsx
@@ -1,9 +1,78 @@
 import React from 'react';
 import styles from './RegisterForm.module.css';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-export const RegisterForm: React.FC = (props) => {
-  console.log(props)
+export const RegisterForm = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <h1>RegisterForm</h1> 
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      className={styles['register-form']}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        label="Confirm Password"
+        name="Confirm"
+        hasFeedback
+        rules={[
+          { required: true, message: 'Please input your Confirm password!' },
+          (({getFieldValue}) => ({
+            validator(_, value) {
+              if(!value || getFieldValue('password') === value) {
+                return Promise.resolve()
+              }
+              return Promise.reject('密码确认不一致')
+            }
+          }))
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
-}
+};
+
+// export const RegisterForm: React.FC = (props) => {
+//   console.log(props)
+//   return (
+//     <h1>RegisterForm</h1> 
+//   );
+// }

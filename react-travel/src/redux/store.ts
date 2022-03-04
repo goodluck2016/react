c45c2ee94cbe5d42dec0cff36b8900f55d203c13
unknown <yumei4466@163.com>
2022-03-04 22:08:24 +0800

    Redux创建 购物车 Slice

--- react-travel/src/redux/store.ts
+++ react-travel/src/redux/store.ts
@@ -9,6 +9,7 @@ import { productSearchSlice } from './productSearch/slice';
 import { userSlice } from './user/slice';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import { shoppingCartSlice } from './shoppingCart/slice';
 
 const persistConfig = {
   key: 'root',
@@ -22,6 +23,7 @@ const rootReducer = combineReducers({
   productDetail: productDetailSlice.reducer,
   productSearch: productSearchSlice.reducer,
   user: userSlice.reducer,
+  shoppingCart: shoppingCartSlice.reducer
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)

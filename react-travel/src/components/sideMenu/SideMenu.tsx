b7d1527d5ba1790e3b43bb93e4d3e807f7139b25
goodluck2016 <zhangyumei_2019@163.com>
2022-10-14 19:00:34 +0800

    update: SideMenu

--- react-travel/src/components/sideMenu/SideMenu.tsx
+++ react-travel/src/components/sideMenu/SideMenu.tsx
@@ -19,7 +19,7 @@ export const SideMenu: React.FC = () => {
         >
           {m.subMenu.map((sm, smindex) => (
             <Menu.SubMenu
-              key={`sub-menu-${smindex}`}
+              key={`sub-menu-${index}-${smindex}`}
               title={
                 <span>
                   <GifOutlined />
@@ -28,7 +28,7 @@ export const SideMenu: React.FC = () => {
               }
             >
               {sm.subMenu.map((sms, smsindex) => (
-                <Menu.Item key={`sub-sub-menu-${smsindex}`}>
+                <Menu.Item key={`sub-sub-menu-${index}-${smindex}-${smsindex}`}>
                   <span>
                     <GifOutlined />
                     {sms}
